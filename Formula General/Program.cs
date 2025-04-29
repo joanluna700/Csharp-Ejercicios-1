fbb5ca9130b8be1a4e6ef7a747431b81561a75d5
Joan Santos <jsantosluna06@gmail.com>
2025-04-29 19:09:07 -0400

    Terminando el ejercicio formula general y modificando el ejercicio multiplicar

--- Formula General/Program.cs
+++ Formula General/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Remoting.Contexts;
 
 namespace Formula_General
 {
@@ -10,6 +7,52 @@ namespace Formula_General
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Solución a ecuaciones cuadráticas mediante la fórmula general");
+
+            //Solicitar al usuario los coeficientes a, b, c
+            Console.WriteLine("Ingrese el valor de a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de c:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            //Conprobar que a no es 0, ya que es una ecuacion cuadratica
+
+            if(a == 0)
+            {
+                Console.WriteLine("El coeficiente 'a' no puede ser 0, ya que es una ecuacion cuadratica");
+                return;
+            }
+
+            //Calculamos el discriminante
+            double discriminante = b * b - 4 * a * c;
+
+
+            //Determinar cual es la naturaleza de las raices
+
+            if(discriminante > 0)
+            {
+
+                //Dos raices reales y distintas
+                double x1 = (-b + Math.Sqrt(discriminante)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminante)) / (2 * a);
+                Console.WriteLine($"Las soluciones reales son: x1 = {Math.Round(x1, 3)}, x2 = {Math.Round(x2, 2)}");
+            }
+            else if(discriminante == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"La solucion real doble es: x = {x}");
+            }
+            else
+            {
+                // Raices complejas
+                double parteReal = -b / (2 * a);
+                double parteImaginaria = Math.Sqrt(-discriminante) / (2 * a);
+                Console.WriteLine($"Las soluciones complejas son: x1 = {parteReal} + {parteImaginaria}i, x2 = {parteReal} - {parteImaginaria}i");
+            }
         }
     }
 }
