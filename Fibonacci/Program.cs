38b3bcd0dd64fadcfd9b5705eeeadd0f8ba357c5
Joan Santos <jsantosluna06@gmail.com>
2025-04-29 20:01:36 -0400

    Terminando ejercicios de numeros impares y secuencia fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables de inicialización
+            int a = 0;
+            int b = 1;
+
+            int c = 0; //Variable de almacenamiento
+
+            Console.WriteLine("Serie de Fibonacci del 1 al 50:");
+
+            while (c <= 50)
+            {
+                if (c >= 1)
+                {
+                    Console.Write(c + " ");
+                }
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            Console.WriteLine(); // Salto de línea al final
+        }
+    }
+}
---
+++ Numeros Impares/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Numeros_Impares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Los numeros impares del 1 al 30 son: ");
+
+            for (int i = 1; i <= 30; i += 2)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
